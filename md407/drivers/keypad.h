3d54846da54fa65b83a07e0f66c7d693c3d9f2c2
Ismail Sacic <cicas03@hotmail.com>
2023-02-12 18:22:19 +0100

    Keypad Driver

---
+++ md407/drivers/keypad.h
@@ -0,0 +1,27 @@
+#include "../types.h"
+#include "../gpio.h"
+/*
+    Keypad driver for the MD407
+    Key mapping:
+    1 2 3 A
+    4 5 6 B
+    7 8 9 C
+    * 0 # D
+    The 4 rows are set to output and the 4 columns are set to input.
+    The columns are set to pull-up and the rows are set to high speed.
+    When a key is pressed, the corresponding column will be pulled down.
+
+    The keypad is connected to GPIOE or GPIOD ( upper or lower )
+*/
+typedef struct Keypad{
+    PGPIO port;
+    boolean upper;
+} Keypad;
+
+// Keypad driver for the MD407
+void keypad_init(Keypad *keypad, PGPIO port, boolean upper);
+char keypad_read_char( Keypad *keypad );
+
+
+// TODO
+// void keypad_set_callback(void (*callback)(uint8_t));
