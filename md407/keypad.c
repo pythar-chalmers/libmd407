a664d64c5936f23e074f7121785b66f46b46056c
Elias Almqvist <elalmqvist@gmail.com>
2023-02-13 16:35:34 +0100

    Refactor stuff

--- md407/keypad.c
+++ md407/keypad.c
@@ -8,23 +8,20 @@
  * @param isHigher If the keypad is connected to the higher 8 pins of the
  * port.
  */
-void keypad_init(Keypad *keypad, PGPIO port, boolean isHigher) {
-	keypad->port  = port;
-	keypad->upper = isHigher;
-
-	uint8_t pin_start = isHigher ? 8 : 0;
+void _Keypad_connect(Keypad *self, PGPIO port) {
+	self->port = port;
 
 	// Set the output pins to high and the input pins to low
-	port->moder = 0x5500 << (pin_start * 2);
+	self->port->moder = 0x5500;
 
 	// Set the output type to push-pull
-	port->otyper = 0x0000 << pin_start;
+	self->port->otyper = 0x0000;
 
 	// Set the speed to high
-	port->ospeedr = 0x5555 << (pin_start * 2);
+	self->port->ospeedr = 0x5555;
 
 	// Set the input type to pull-up
-	port->pupdr = 0x0066 << (pin_start * 2);
+	self->port->pupdr = 0x0066;
 }
 
 /**
@@ -34,31 +31,20 @@ void keypad_init(Keypad *keypad, PGPIO port, boolean isHigher) {
  * @return The key that is currently pressed. If no key is pressed, 0xFF is
  * returned.
  */
-char keypad_read_char(Keypad *keypad) {
-
-	char keys[4][4] = {{'1', '2', '3', 'A'},
-	                   {'4', '5', '6', 'B'},
-	                   {'7', '8', '9', 'C'},
-	                   {'*', '0', '#', 'D'}};
-
-	uint8_t pin_start = keypad->upper ? 8 : 0;
+char _Keypad_read(Keypad *self) {
+	keypad->port->odr = 0;
 
 	for (uint8_t i = 0; i < 4; i++) {
-		keypad->port->odr = 0x0000 << pin_start;
-		keypad->port->odr = 0x10 << (i + pin_start);
+		keypad->port->odr = 0x10 << i;
 
 		for (uint8_t j = 0; j < 4; j++) {
-			if (keypad->upper) {
-				if (keypad->port->idr_high & 0x1 << j) {
-					return keys[i][j];
-				}
-			} else {
-				if (keypad->port->idr_low & 0x1 << j) {
-					return keys[i][j];
-				}
+			if (keypad->port->idr_high & (1 << j)) {
+				keypad->port->odr = 0;
+				return keypad->map[i][j];
 			}
 		}
 	}
 
+	keypad->port->odr = 0;
 	return 0xFF;
 }
--- md407/keypad.h
+++ md407/keypad.h
@@ -13,14 +13,23 @@
 
     The keypad is connected to GPIOE or GPIOD ( upper or lower )
 */
-typedef struct {
+
+// Defualt key mapping
+static char KEYPAD_MAP[4][4] = {{'1', '2', '3', 'A'},
+                                {'4', '5', '6', 'B'},
+                                {'7', '8', '9', 'C'},
+                                {'*', '0', '#', 'D'}};
+
+typedef struct Keypad {
 	PGPIO port;
-	boolean upper;
+	char *map[4][4];
+
+	void (*connect)(Keypad* self, PGPIO port); // Init
+	char (*read)(Keypad* self); // Read from port
 } Keypad;
 
-// Keypad driver for the MD407
-void keypad_init(Keypad *keypad, PGPIO port, boolean upper);
-char keypad_read_char(Keypad *keypad);
+void _Keypad_connect(Keypad* self, PGPIO port);
+char _Keypad_read(Keypad* self);
 
 // TODO
 // void keypad_set_callback(void (*callback)(uint8_t));
