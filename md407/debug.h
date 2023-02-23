3f364b7b193cfe3dd513404cd4294aedc9a4f8a2
Elias Almqvist <elalmqvist@gmail.com>
2023-02-23 10:37:14 +0100

    Refactor

--- md407/debug.h
+++ md407/debug.h
@@ -1,31 +1,33 @@
 #pragma once
 
+#include "types.h"
+
 #define _USART_BASE 0x40011000
 #define USART(n)    ((PUSART) _USART_BASE + 0x1400 * n)
 
 void printc(char *s);
 void print_usart(char *s, unsigned char id);
 
-typedef struct {
-	volatile unsigned short sr;
-	volatile unsigned short Unused0;
-	volatile unsigned short dr;
-	volatile unsigned short Unused1;
-	volatile unsigned short brr;
-	volatile unsigned short Unused2;
-	volatile unsigned short cr1;
-	volatile unsigned short Unused3;
-	volatile unsigned short cr2;
-	volatile unsigned short Unused4;
-	volatile unsigned short cr3;
-	volatile unsigned short Unused5;
-	volatile unsigned short gtpr;
+typedef volatile struct {
+	uint16_t sr;
+	uint16_t Unused0;
+	uint16_t dr;
+	uint16_t Unused1;
+	uint16_t brr;
+	uint16_t Unused2;
+	uint16_t cr1;
+	uint16_t Unused3;
+	uint16_t cr2;
+	uint16_t Unused4;
+	uint16_t cr3;
+	uint16_t Unused5;
+	uint16_t gtpr;
 } USART, *PUSART;
 
 static void print_char(char c, unsigned char id) {
 	while ((USART(id)->sr & 0x80) == 0)
 		;
-	USART(id)->dr = (unsigned short) c;
+	USART(id)->dr = (uint16_t) c;
 	if (c == '\n')
 		print_char('\r', id);
 }
