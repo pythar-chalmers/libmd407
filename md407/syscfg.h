fabc6b8de566b453ea11f6a8b0eed8e6f60e5920
Elias Almqvist <elalmqvist@gmail.com>
2023-03-01 12:23:00 +0100

    SYSCFG struct (#13)

---
+++ md407/syscfg.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "mem.h"
+#include "types.h"
+
+#define _SYSCFG_EXTI_DT_16_R(name)                                           \
+	union {                                                                  \
+		struct {                                                             \
+			uint16_t name;                                                   \
+			unsigned : 16;                                                   \
+		};                                                                   \
+		struct {                                                             \
+			unsigned name##_0 : 4;                                           \
+			unsigned name##_1 : 4;                                           \
+			unsigned name##_2 : 4;                                           \
+			unsigned name##_3 : 4;                                           \
+			unsigned          : 16;                                          \
+		};                                                                   \
+	}
+
+typedef struct {
+	// MEMRMP
+	union {
+		uint32_t memrmp;
+		struct {
+			unsigned memrmp_0 : 1;
+			unsigned memrmp_1 : 1;
+			unsigned memrmp_2 : 1;
+			unsigned          : 5;
+			unsigned memrmp_3 : 1;
+			unsigned          : 1;
+			unsigned memrmp_4 : 1;
+			unsigned memrmp_5 : 1;
+		};
+	};
+
+	// PMC
+	union {
+		uint32_t pmc;
+		struct {
+			unsigned         : 16;
+			unsigned ADCxDC2 : 3;
+			unsigned         : 4;
+			unsigned R_SEL   : 1;
+		};
+	};
+
+	// EXTICR
+	_SYSCFG_EXTI_DT_16_R(exticr1);
+	_SYSCFG_EXTI_DT_16_R(exticr2);
+	_SYSCFG_EXTI_DT_16_R(exticr3);
+	_SYSCFG_EXTI_DT_16_R(exticr4);
+
+	// CMPCR
+	union {
+		uint32_t cmpcr;
+		struct {
+			unsigned cmpcr_0 : 1;
+			unsigned         : 7;
+			unsigned cmpcr_1 : 1;
+		};
+	};
+} *PSYSCFG;
+
+#define SYSCFG ((PSYSCFG) 0x40013800)
